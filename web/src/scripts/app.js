192ea1d87d4790672a3fc298fe34065c12a025e8
mikehwagz <mike@selfaware.studio>
2020-05-04 16:16:26 -0400

    feat(web): marquee prototype

--- web/src/scripts/app.js
+++ web/src/scripts/app.js
@@ -2,9 +2,11 @@ import { picoapp } from 'picoapp'
 import { size } from '@selfaware/martha'
 
 import mark from '@/components/mark'
+import marquee from '@/components/marquee'
 
 const components = {
   mark,
+  marquee,
 }
 
 const state = {
