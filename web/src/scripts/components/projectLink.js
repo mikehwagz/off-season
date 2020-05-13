d7282994d0e60b5141aa860e2fe53f332c849dc1
mikehwagz <mike@selfaware.studio>
2020-05-13 10:14:18 -0400

    feat(web): update link style on enter only

--- web/src/scripts/components/projectLink.js
+++ web/src/scripts/components/projectLink.js
@@ -4,7 +4,15 @@ import gsap from 'gsap'
 
 export default component((node, ctx) => {
   let thumbs = document.querySelectorAll('.js-thumb')
+  let links = document.querySelectorAll('.js-links')
+
   let offEnter = on(node, 'mouseenter', () => {
+    links.forEach((link) => {
+      gsap.set(link, {
+        color: link === node ? node.dataset.color : '#000',
+      })
+    })
+
     thumbs.forEach((img) => {
       gsap.set(img, {
         autoAlpha: img.dataset.id === node.getAttribute('id') ? 1 : 0,
