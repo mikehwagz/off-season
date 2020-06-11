3187ffd17417c7f5c342f27014d0b54bfbcbb581
mikehwagz <mike@selfaware.studio>
2020-06-11 17:17:53 -0400

    fix(web): add everything back in

--- web/src/scripts/renderers/Base.js
+++ web/src/scripts/renderers/Base.js
@@ -3,7 +3,7 @@ import { on, size, add, remove } from '@selfaware/martha'
 import gsap from 'gsap'
 import app from '@/app'
 import { listen } from 'quicklink'
-// import loadFonts from '@/lib/loadFonts'
+import loadFonts from '@/lib/loadFonts'
 
 class Base extends Highway.Renderer {
   onFirstLoad() {
@@ -18,16 +18,14 @@ class Base extends Highway.Renderer {
     // setup render loop
     gsap.ticker.add(this.tick)
 
-    // mount app
-    this.mount()
-
     gsap.set('[data-router-view]', { autoAlpha: 1 })
 
-    // loadFonts().then(() => {
-
-    //   // animate in
-    //   remove(document.body, 'o0')
-    // })
+    loadFonts().then(() => {
+      // mount app
+      this.mount()
+      // animate in
+      remove(document.body, 'o0')
+    })
   }
 
   onEnter() {}
