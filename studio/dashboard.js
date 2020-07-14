4b89d6d0479c90cedf90af290bafdd0985de3f84
mikehwagz <mike@selfaware.studio>
2020-07-14 08:49:30 -0400

    feat(studio): update netlify dashboard widget config

--- studio/dashboard.js
+++ studio/dashboard.js
@@ -7,8 +7,8 @@ export default {
         sites: [
           {
             title: 'Off Season',
-            apiId: 'bb8bf8b4-18a3-49e5-970e-b350b580cc15',
-            buildHookId: '5ed005de750908d1b995664f',
+            apiId: 'fd685ac5-8d6f-4fb8-b064-c5bdbea937f3',
+            buildHookId: '5f0da9a42f6ebec2fe72a9af',
             name: 'offseason',
           },
         ],
